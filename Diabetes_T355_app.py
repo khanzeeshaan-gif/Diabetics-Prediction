e90c3c3b13f8993122a20fb53a8b7180a2aa8ad5
Zeeshaan Khan <khanzeeshaan4@gmail.com>
2026-04-10 15:59:30 +0530

    Update Diabetes_T355_app.py

--- Diabetes_T355_app.py
+++ Diabetes_T355_app.py
@@ -26,7 +26,7 @@ if st.button("Predict"):
     # 4. Scale and Predict
     scaled_features = scaler.transform(features)
     prediction = model.predict(scaled_features)
-st.success(f"This is pred, {prediction[0]"}
+st.success(f"This is pred, {prediction[0]}")
     # if prediction[0] == 1:
     #     st.error("The model predicts a high risk of diabetes.")
     # else:
